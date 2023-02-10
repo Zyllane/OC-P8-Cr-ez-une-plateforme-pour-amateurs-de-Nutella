867d09d2adae50162b9b76ea53e94732eade2526
zyllane <ddamaro@outlook.fr>
2023-02-10 17:34:37 +0100

    Création des tables dans la DB

--- P8/db/models.py
+++ P8/db/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-class Teacher(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=80)
-    age = models.IntegerField()
+    nutriscore = models.CharField(max_length=1)
+
+class User(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=16)
+
+    """Vérifier best practice Django vis à vis des mdp"""
+
+class Bookmark(models.Model):
+    id_product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
