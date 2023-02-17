4b7036d226446567492095593e41f3c32696be47
zyllane <ddamaro@outlook.fr>
2023-02-17 17:06:19 +0100

    Création des tables dans la DB

--- P8/db/urls.py
+++ P8/db/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
 
 urlpatterns = [
-    path('db/', include('db.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
--- P8/db/views.py
+++ P8/db/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template("main/index.html")
+    test = "mefaitplaisir"
+    context = {
+        'test1': test
+    }
+    return HttpResponse(template.render(context))
